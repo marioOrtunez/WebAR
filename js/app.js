c359b3a56b087541fac34bd11906a32676046978
marioOrtunez <162109593+marioOrtunez@users.noreply.github.com>
2024-04-08 15:51:08 +0200

    nueva prueba

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build2.json");
 let WebXR;
 window.ARWT = {}
 
