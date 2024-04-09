e94322fbe6bf86de48b4004cdc4fe4ac8d30c9d0
marioOrtunez <162109593+marioOrtunez@users.noreply.github.com>
2024-04-09 14:41:49 +0200

    prueba del ave

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/BuildNoURP.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/buildAve.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
