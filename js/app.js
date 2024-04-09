e125827ae57a340c55f89109e09f16c1badba3a6
marioOrtunez <162109593+marioOrtunez@users.noreply.github.com>
2024-04-09 11:14:20 +0200

    vuelta a la prueba inicial

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/BuildMexicana2.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Builds.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
