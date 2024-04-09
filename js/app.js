000710e694bbfcf5cc26d2536afd34da258f9477
marioOrtunez <162109593+marioOrtunez@users.noreply.github.com>
2024-04-09 09:26:48 +0200

    vuelta a la prueba mexicana

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/BuildEarth.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/BuildMexican.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
