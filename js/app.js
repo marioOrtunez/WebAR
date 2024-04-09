f2788136f9d23f64e50e7b80e147f4551bbe07a6
marioOrtunez <162109593+marioOrtunez@users.noreply.github.com>
2024-04-09 13:40:07 +0200

    sgfdvsg

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/buildTerra.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/buildEjem.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
