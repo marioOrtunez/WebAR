ecdb4a45c978dc5e30c4f2761f4d9e03b84ed475
marioOrtunez <162109593+marioOrtunez@users.noreply.github.com>
2024-04-09 09:21:53 +0200

    prueba de la tierra

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Builds.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/BuildEarth.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
